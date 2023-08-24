b43c0d150254ee65f68f235de05337b36e8a900e
Clust34 <chapuisnicolas34@gmail.com>
2023-08-24 09:45:21 +0200

    fix: sanitizer html all page

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -4,15 +4,16 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Regex;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\Length;
-use Symfony\Component\Validator\Constraints\NotBlank;
 
 
 class RegistrationFormType extends AbstractType
@@ -29,32 +30,40 @@ class RegistrationFormType extends AbstractType
             ])
             ->add('password', RepeatedType::class, [
                 'type' => PasswordType::class,
+                'invalid_message' => 'Les mots de passe doivent correspondre',
+                // instead of being set onto the object directly,
+                // this is read and encoded in the controller
+                'mapped' => false,
                 'first_options' => [
-                    'attr' => [
-                        'autocomplete' => 'new-password',
-                        'placeholder' => 'S3CR3T'
-                    ],
-                    'constraints' => [
-                        new NotBlank([
-                            'message' => 'Entrer un mot de passe',
-                        ]),
-                        new Length([
-                            'min' => 6,
-                            'minMessage' => 'Votre mot de passe dois faire plus de {{ limit }} caractères.',
-                            'max' => 4096,
-                        ]),
-                    ],
-                    'label' => 'Mot de passe : ',
-                ],
-                'second_options' => [
+                    'label' => 'Mot de passe:',
+                    'required' => true,
                     'attr' => [
                         'autocomplete' => 'new-password',
                         'placeholder' => 'S3CR3T',
                     ],
-                    'label' => 'Répéter le mot de passe : ',
+                    'constraints' => [
+                        new NotBlank([
+                            'message' => 'Veuillez renter un mot de passe',
+                        ]),
+                        new Length([
+                            'min' => 6,
+                            'minMessage' => 'Votre mot de passe doit être supérieur à {{ limit }} caractères.',
+                            // max length allowed by Symfony for security reasons
+                            'max' => 4096,
+                        ]),
+                        new Regex(
+                            pattern: '/^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[^\w\d\s:])([^\s]){8,16}$/',
+                            message: 'Votre mot de passe doit contenir au moins 1 nombre, 1 lettre en minuscule, 1 lettre en majuscule et un caractère spécial',
+                        ),
+                    ],
                 ],
-                'invalid_message' => 'Les mots de passe ne correspondent pas.',
-                'mapped' => false,
+                'second_options' => [
+                    'label' => 'Confirmation du mot de passe:',
+                    'attr' => [
+                        'placeholder' => 'S3CR3T',
+                    ],
+                    'required' => true,
+                ],
             ])
             ->add('prenom', TextType::class, [
                 'label' => 'Votre prénom : ',
