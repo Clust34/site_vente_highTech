221787703db3b1cc0564081cda9b2ab19a1b299d
Clust34 <chapuisnicolas34@gmail.com>
2023-09-12 14:07:00 +0200

    Feat: add animation JS and SCSS

--- assets/app.js
+++ assets/app.js
@@ -8,6 +8,11 @@
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.scss';
 
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
+// ..
+AOS.init();
+
 // Import Javascript
 import './js/addCollectionInput';
 import './js/swiperDetail';
